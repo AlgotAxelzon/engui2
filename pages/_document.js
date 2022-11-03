6cdcf4afe4b4363cb53c53e02a5a0779eac2e33e
real-linneus <51710017+real-linneus@users.noreply.github.com>
2022-11-03 20:20:46 +0100

    Adsense script

--- pages/_document.js
+++ pages/_document.js
@@ -25,6 +25,8 @@ class MyDocument extends Document {
                         `,
                         }}
                     />
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7796283578510574"
+                        crossorigin="anonymous"></script>
                 </Head>
                 <body>
                     <Main />
